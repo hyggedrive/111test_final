9937a0bfe9cafbb131a50695a4968e3a4a35f702
hyggedrive <z1042488527@163.com>
2026-07-23 16:58:30 +0800

    llvm: avoid std::filesystem for GCC 7 compatibility

--- llvm/lib/LTO/LTOBackend.cpp
+++ llvm/lib/LTO/LTOBackend.cpp
@@ -52,7 +52,6 @@
 #include "llvm/Transforms/Utils/FunctionImportUtils.h"
 #include "llvm/Transforms/Utils/SplitModule.h"
 #include "llvm/Transforms/Utils/SplitModuleCG.h"
-#include <filesystem>
 #include <optional>
 
 using namespace llvm;
@@ -139,10 +138,9 @@ Error Config::addSaveTemps(std::string OutputFileName, bool UseInputModulePath,
       // through.
       if (LinkerHook && !LinkerHook(Task, M))
         return false;
-
+	
       auto extract_filename = [](const std::string &path) -> std::string {
-        std::filesystem::path fs_path(path);
-        return fs_path.filename().string();
+	        return llvm::sys::path::filename(path).str();
       };
 
       std::string PathPrefix;
